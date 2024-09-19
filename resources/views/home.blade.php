737f951d29a9f2fc191419802c1c61f082f6e444
unknown <navithania@gmail.com>
2024-09-19 09:42:25 +0700

    Update: folder structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="css/style.css">
+
+<!-- 
+    <style>
+        body {
+            background-color: peachpuff;
+        }
+    </style> 
+-->
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <br>
+    <h1>Halaman Home</h1>
+
+    <script src="js/script.js"></script>
+</body>
+</html>
