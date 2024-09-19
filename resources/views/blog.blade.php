073dee1ea4a1749a6e197c165c73aa43bf11b9ce
unknown <navithania@gmail.com>
2024-09-19 11:20:06 +0700

    Update: blade templating

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <br>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates ut culpa, itaque, fugit id distinctio alias esse aliquam, minus neque quasi adipisci a doloribus molestiae temporibus ab consectetur quas pariatur. </p>
+    </article>
+
+    <article>
+        <h3>Judul 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti libero assumenda cupiditate aspernatur voluptatibus! Aperiam necessitatibus et, porro ipsa ducimus adipisci corrupti doloremque maxime, sit nam quos culpa sint harum! </p>
+    </article>
+</body>
+</html>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman About</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Halaman Contact</title>
 </head>
 <body>
     <a href="/">Home</a>
@@ -13,8 +12,12 @@
     <a href="/about">About</a>
     <a href="/contact">Contact</a>
     <br>
-    <h1>Halaman About :)</h1>
-    <h3>Owner: {{ $nama }}</h3>
-    <img src="img/balloon.gif" alt="hi" width="200">
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            <a href="instagram.com/navithania">Instagram</a>
+        </li>
+    </ul>
+    
 </body>
 </html>
