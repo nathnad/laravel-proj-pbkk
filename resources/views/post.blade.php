e31d8119ba15642e3ffe63995c08cd41aeb84886
unknown <navithania@gmail.com>
2024-09-25 09:28:48 +0700

    Update: view data

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,8 +1,7 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <br>
   
-    <article class="py-8 max-w-screen-md">
+    <article class="py-7 max-w-screen-md">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
             <a>{{ $post['author'] }}</a> | 1 Januari 2024
@@ -11,5 +10,4 @@
         <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
     </article>
 
-    <br>
 </x-layout>
